ffdba7c3235fdf9e29c16fc1759e067d535dbee9
ochman <ochmankuba@gmail.com>
2023-12-02 14:25:11 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use("/", (req, res) => {
-    res.send("Server is running")
-})
+// app.use("/", (req, res) => {
+//     res.send("Server is running")
+// })
 
 app.use("/test", (req, res) => {
     res.send("test")
