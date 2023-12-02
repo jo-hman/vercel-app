cd80a58c513771c09efdd0edf6fa7faefb57a95f
ochman <ochmankuba@gmail.com>
2023-12-02 14:20:21 +0100

    test

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ app.use("/", (req, res) => {
     res.send("Server is running")
 })
 
+app.use("/costam", (req, res) => {
+    res.send("costam")
+})
+
 app.listen(5000, console.log("Server started on port 5000"));
